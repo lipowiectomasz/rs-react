331bbebe40d40e3208fa9a832e113cfa10c19827
Tomasz Lipowiec <lipowiectomasz@gmail.com>
2025-02-05 01:04:31 +0100

    fix: proper expose of app

--- rs-react-app/vite.config.ts
+++ rs-react-app/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allow external access
+    port: 5173, // Explicitly set the port (optional)
+    strictPort: true,
+  },
 });
